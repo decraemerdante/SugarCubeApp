75e94f80d195345f14bfe140c9be8bca30a37f65
Ddc <decraemerdante@gmail.com>
2017-10-24 21:25:52 +0200

    gui stuff

--- SugarCube/SugarCube/www/scripts/view.js
+++ SugarCube/SugarCube/www/scripts/view.js
@@ -4,20 +4,22 @@ function fillInTable(data) {
     var l = data.length;
     var div = $("#waardeTable");
 
-    var tableString = "<thead><tr>" +
-        "<th data-priority='1'>Time</th>" +
-        "<th data-priority='persistent'>Value</th>" +       
-        "</tr></thead><tbody>";
+    var tableString = "<tbody>";
+         
+        
 
     var groupByDate = groupByDateNow(data);
     console.log(groupByDate);
     Object.keys(groupByDate).forEach(function (category) {
-        tableString += "<tr><td class='dates' colspan='2'>" + getDate(category) + "</td></tr>";
-
+        tableString += "<tr><td class='dates' colspan='4'>" + getDate(category) + "</td></tr>";
+        tableString += "<tr><td></td><td></td><td>Bolus</td><td>Basal</td></tr>";
         groupByDate[category].forEach(function (memb, i) {
-
-            tableString += "<tr><td>" + getTime(memb.time) + "</td>" +
-                "<td>" + memb.waarde1 + "</td></tr>"
+            
+            tableString += "<tr><td>" + getTime(memb.time) + "</td>" +                
+                "<td>" + memb.waarde1 + "</td>" +
+                "<td>" + memb.bolus + "</td>" +
+                "<td>" + memb.basal + "</td>" +
+                "</tr > "
 
         });
     });
@@ -60,6 +62,13 @@ function getTime(dateTime) {
 function getDate(date) {
     var myDate = date.substr(0, 10);
 
-    return myDate;
+    return getDateFull(myDate);
 }
 
+var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+function getDateFull(date) {
+    return(days[date.getDay()] + ' ' + months[date.getMonth()] + ' ' + date.getDate() + ' ' + date.getFullYear()); //Tuesday February 12 2013
+
+
+}
